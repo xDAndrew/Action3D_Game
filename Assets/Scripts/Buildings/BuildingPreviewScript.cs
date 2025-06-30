af50be9a1f913048720a51ee10d578e1330fe4ec
xDAndrew <xD.Andrew2012@yandex.by>
2025-06-30 09:20:36 +0200

    Continue course

---
+++ Assets/Scripts/Buildings/BuildingPreviewScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Buildings
+{
+    public class BuildingPreviewScript : MonoBehaviour
+    {
+        public Material canPlaceMaterial;
+        public Material canNotPlaceMaterial;
+    }
+}
---
+++ Assets/Scripts/Data/Building/BuildingRecipeData.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Data.Crafting;
+using UnityEngine;
+
+namespace Data.Building
+{
+    [CreateAssetMenu(fileName = "BuildingRecipeData", menuName = "Scriptable Objects/BuildingRecipeData")]
+    public class BuildingRecipeData : ScriptableObject
+    {
+        public string DisplayName;
+        public Sprite Icon;
+        
+        public GameObject spawnPrefab;
+        public GameObject previewPrefab;
+        public List<CraftingRecipe> resourceCost;
+    }
+}
